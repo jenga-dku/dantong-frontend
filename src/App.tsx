c360b5f6e141dd192e264eaeb6f88a27d77a1944
gaeunnlee <gaeunnnleee@gmail.com>
2024-04-25 16:39:29 +0900

    design: 모바일 레이아웃 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import Router from './Router';
 
 function App() {
-   return <Router />;
+   return (
+      <div className="absolute flex w-full max-w-[480px] h-full bg-white">
+         <Router />
+      </div>
+   );
 }
 
 export default App;
