c5f0a83829baaaae394a2574293e840c512773bb
gaeunnlee <gaeunnnleee@gmail.com>
2024-04-25 17:24:57 +0900

    chore: Tailwind CSS 클래스명 정렬 Prettier Plugin 설치

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Router from './Router';
 
 function App() {
-   return (
-      <div className="absolute flex w-full max-w-[480px] h-full bg-white">
-         <Router />
-      </div>
-   );
+  return (
+    <div className="absolute flex h-full w-full max-w-[480px] bg-white">
+      <Router />
+    </div>
+  );
 }
 
 export default App;
