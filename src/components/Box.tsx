a033e7bf127a94841c5a8e9a393652129f5f21d3
gaeunnlee <gaeunnnleee@gmail.com>
2024-05-04 02:08:12 +0900

    feat: Box 컴포넌트 중복 클래스명 제거 함수 적용

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,14 +1,18 @@
 import { HTMLAttributes, ReactNode } from 'react';
+import { handleCustomCSS } from '../utils/handleCustomCSS';
 
 type Box = HTMLAttributes<HTMLDivElement> & {
   children: ReactNode;
   className?: string;
 };
 
+export const boxStyle =
+  'flex w-full rounded-[10px] bg-white p-5 shadow-[1px_2px_10px_0px_rgba(0,0,0,0.1)]';
+
 export const Box = ({ children, className, ...props }: Box) => {
   return (
     <div
-      className={`flex w-full rounded-[10px] bg-white p-5 shadow-[1px_2px_10px_0px_rgba(0,0,0,0.1)] ${className}`}
+      className={className ? handleCustomCSS(boxStyle, className) : boxStyle}
       {...props}
     >
       {children}
