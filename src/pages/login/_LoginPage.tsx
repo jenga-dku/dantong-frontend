72016eb93265a3efa15b5b137ec5f987565cb750
gaeunnlee <gaeunnnleee@gmail.com>
2024-05-08 00:51:08 +0900

    feat: 로그인 태그 변경

--- src/pages/login/_LoginPage.tsx
+++ src/pages/login/_LoginPage.tsx
@@ -7,7 +7,7 @@ export const LoginPage = () => {
 
   return (
     <div className="mt-[-5rem] flex w-full flex-col items-center justify-center">
-      <div className="flex h-fit w-full flex-col items-center gap-5 p-5 text-[#C4C4C4]">
+      <form className="flex h-fit w-full flex-col items-center gap-5 p-5 text-[#C4C4C4]">
         <h1 className="font-SejongHospitalBold text-4xl text-primary">단통</h1>
         <Input label="Student ID" maxLength={8}>
           <span>@dankook.ac.kr</span>
@@ -20,7 +20,7 @@ export const LoginPage = () => {
           }}
           content="로그인"
         />
-      </div>
+      </form>
     </div>
   );
 };
