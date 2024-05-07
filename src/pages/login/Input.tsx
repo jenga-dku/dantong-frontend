bee50340923f3e548e6c86fc7b3033d6656c8eae
gaeunnlee <gaeunnnleee@gmail.com>
2024-05-08 00:37:24 +0900

    feat: 로그인 페이지 퍼블리싱

---
+++ src/pages/login/Input.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react';
+
+export const Input = ({
+  label,
+  children,
+  maxLength,
+  type,
+}: {
+  label: string;
+  children?: ReactNode;
+  maxLength?: number;
+  type?: string;
+}) => {
+  return (
+    <label className="flex h-[3.2rem] w-full items-center justify-between rounded-lg p-3 shadow-[1px_0.5px_1px_0.3px_rgba(0,0,0,0.2)]">
+      <p className="absolute ml-2 mt-[-3rem] text-sm">{label}</p>
+      <input
+        type={type ?? 'text'}
+        className="bg-white px-1 text-black"
+        maxLength={maxLength}
+      />
+      {children}
+    </label>
+  );
+};
--- src/pages/login/_LoginPage.tsx
+++ src/pages/login/_LoginPage.tsx
@@ -1,3 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from '../../components/Button';
+import { Input } from './Input';
+
 export const LoginPage = () => {
-   return <>Login</>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="mt-[-5rem] flex w-full flex-col items-center justify-center">
+      <div className="flex h-fit w-full flex-col items-center gap-5 p-5 text-[#C4C4C4]">
+        <h1 className="font-SejongHospitalBold text-4xl text-primary">단통</h1>
+        <Input label="Student ID" maxLength={8}>
+          <span>@dankook.ac.kr</span>
+        </Input>
+        <Input label="Password" type="password" />
+        <Button
+          size="full"
+          onClick={() => {
+            navigate('/');
+          }}
+          content="로그인"
+        />
+      </div>
+    </div>
+  );
 };
