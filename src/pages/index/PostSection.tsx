cc01539bc8ea6e67dc0f3bbd3ac4f95cfb951ccd
gaeunnlee <gaeunnnleee@gmail.com>
2024-06-02 03:05:07 +0900

    chore: console.log 삭제

--- src/pages/index/PostSection.tsx
+++ src/pages/index/PostSection.tsx
@@ -4,15 +4,10 @@ import { PostDataList } from '../../data';
 import { PostItem } from './PostItem';
 import { Section } from './Section';
 import { useGetPostList } from '../../query-hooks/post';
-import { useEffect } from 'react';
 import { PostDetailResponse } from '../../api/post/types';
 
 export const PostSection = () => {
-  const { data, isLoading } = useGetPostList('EVENT');
-  useEffect(() => {
-    console.log(isLoading);
-    console.log(data);
-  }, [data, isLoading]);
+  const { data } = useGetPostList('');
 
   return (
     <Section title="게시글 둘러보기">
