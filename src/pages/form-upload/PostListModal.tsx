741adecdb12c6ec7770863e489425a6f4b665159
gaeunnlee <gaeunnnleee@gmail.com>
2024-06-04 06:19:03 +0900

    feat: react-query Infinite Scroll 구현

--- src/pages/form-upload/PostListModal.tsx
+++ src/pages/form-upload/PostListModal.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Button } from '../../components/Button';
-import { useGetPostList } from '../../query-hooks/post';
+import { useGetInfinitePostList } from '../../query-hooks/post';
 import { getCategoryKoreanName } from '../../utils/getCategoryKoreanName';
 import { FormUpload } from '../../api/form-upload/types';
+import { useIntersectionObserver } from '../../hooks/useIntersectionObserver';
+import { PostDetailResponse } from '../../api/post/types';
 
 export type PostModalState = {
   visible: boolean;
@@ -16,45 +18,55 @@ export const PostListModal = ({
   updatePostModalState: React.Dispatch<React.SetStateAction<PostModalState>>;
   updateFormUploadInfo: React.Dispatch<React.SetStateAction<FormUpload>>;
 }) => {
-  const { data: postList } = useGetPostList('');
+  const postListState = useGetInfinitePostList({
+    category: '',
+    size: 5,
+  });
+
+  const {
+    list: postList,
+    intersection,
+    isFetching,
+  } = useIntersectionObserver<PostDetailResponse>(postListState);
+
   return (
     <div className="absolute left-0 top-0 z-[51] flex  h-full w-full flex-col items-center justify-center bg-[#00000080]">
       <div className="flex h-[300px] w-[calc(100%-30px)] flex-col justify-center gap-2 overflow-hidden rounded-2xl bg-white p-5">
         <p className="w-full text-center font-bold">글 선택</p>
         <div className="flex h-[280px] w-full flex-col gap-4 overflow-y-scroll p-2">
-          {postList?.content.map(
-            ({ postId, title, category, postFileResponse }) => (
-              <div
-                key={`Post-Modal-${postId}`}
-                onClick={() => {
-                  updateFormUploadInfo((prev) => ({ ...prev, postId }));
-                  updatePostModalState((prev) => ({
-                    selectedPostTitle: title,
-                    visible: false,
-                  }));
-                }}
-                className="flex cursor-pointer items-center gap-4"
-              >
-                {postFileResponse.length > 0 ? (
-                  <img
-                    className="h-[50px] w-[50px] rounded-md border-[1px] border-solid border-zinc-300"
-                    src={postFileResponse[0].url ?? ''}
-                    alt={postFileResponse[0].originalName ?? ''}
-                  />
-                ) : (
-                  <div className="h-[50px] w-[50px] rounded-md bg-slate-200" />
-                )}
-                <div className="flex w-[calc(100%-60px)] flex-col gap-1 overflow-hidden ">
-                  <p className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm">
-                    {title}
-                  </p>
-                  <p className="text-xs text-primary">
-                    {getCategoryKoreanName(category)}
-                  </p>
-                </div>
+          {postList?.map(({ postId, title, category, postFileResponse }) => (
+            <div
+              key={`Post-Modal-${postId}`}
+              ref={intersection}
+              onClick={() => {
+                updateFormUploadInfo((prev) => ({ ...prev, postId }));
+                updatePostModalState((prev) => ({
+                  selectedPostTitle: title,
+                  visible: false,
+                }));
+              }}
+              className="flex cursor-pointer items-center gap-4"
+            >
+              {postFileResponse.length > 0 ? (
+                <img
+                  className="h-[50px] w-[50px] rounded-md border-[1px] border-solid border-zinc-300"
+                  src={postFileResponse[0].url ?? ''}
+                  alt={postFileResponse[0].originalName ?? ''}
+                />
+              ) : (
+                <div className="h-[50px] w-[50px] rounded-md bg-slate-200" />
+              )}
+              <div className="flex w-[calc(100%-60px)] flex-col gap-1 overflow-hidden ">
+                <p className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+                  {title}
+                </p>
+                <p className="text-xs text-primary">
+                  {getCategoryKoreanName(category)}
+                </p>
               </div>
-            ),
-          )}
+            </div>
+          ))}
+          {isFetching && <div>로딩 중..</div>}
         </div>
         <Button
           content="확인"
--- src/query-hooks/post/index.ts
+++ src/query-hooks/post/index.ts
@@ -1,10 +1,27 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import { Post } from '../../api/post';
+import { FilterCategory } from '../../types/news-category';
 
 export const useGetPostList = (category: string) =>
   useQuery({
     queryFn: () => Post.getPostList(category),
     queryKey: ['postList'],
+  });
+
+export const useGetInfinitePostList = ({
+  category,
+  size,
+}: {
+  size: number;
+  category: FilterCategory;
+}) =>
+  useInfiniteQuery({
+    queryKey: ['infinitePostList'],
+    queryFn: ({ pageParam: pageNum }) =>
+      Post.getInfinitePostList({ page: pageNum, size, category }),
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, allPages) =>
+      lastPage.length ? allPages.length + 1 : undefined,
   });
 
 export const useGetPostDetail = (id: number) =>
