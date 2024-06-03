c06168a66f9177ebf4a9dd3b4fc7b42c9f42390a
gaeunnlee <gaeunnnleee@gmail.com>
2024-06-04 03:19:32 +0900

    feat: 소식 페이지 폼 신청 버튼 표시 로직 수정

--- src/pages/post/_PostPage.tsx
+++ src/pages/post/_PostPage.tsx
@@ -27,15 +27,19 @@ export const PostPage = () => {
           />
           <hr />
           <p className="p-4 text-sm">{parse(data.content)}</p>
-          <div className=" p-4 pt-2">
-            <Button
-              content="신청"
-              size="full"
-              onClick={() => {
-                navigate(`/form/${data.surveyId}`);
-              }}
-            />
-          </div>
+          {data.surveyId ? (
+            <div className=" p-4 pt-2">
+              <Button
+                content="신청"
+                size="full"
+                onClick={() => {
+                  navigate(`/form/${data.surveyId}`);
+                }}
+              />
+            </div>
+          ) : (
+            <div className="h-8" />
+          )}
         </Box>
       )}
     </div>
