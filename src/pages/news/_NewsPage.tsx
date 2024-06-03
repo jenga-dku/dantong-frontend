2f7e1ac4aea833457b0ced439a347bd1e84e463f
gaeunnlee <gaeunnnleee@gmail.com>
2024-06-04 07:16:55 +0900

    feat: 소식 페이지 Infinite Scroll 적용

--- src/pages/news/_NewsPage.tsx
+++ src/pages/news/_NewsPage.tsx
@@ -1,16 +1,26 @@
 import { useEffect } from 'react';
-import { useGetPostList } from '../../query-hooks/post';
+import { useGetInfinitePostList } from '../../query-hooks/post';
 import { FilterNav } from './FilterNav';
 import { PostItem } from './PostItem';
 import { PostDetailResponse } from '../../api/post/types';
 import { useSearchParams } from 'react-router-dom';
+import { isInCategory } from '../../utils/isInCategory';
+import { useIntersectionObserver } from '../../hooks/useIntersectionObserver';
+import { Intersection } from '../../components/Intersection';
 
 export const NewsPage = () => {
   const [searchParams] = useSearchParams();
-  let category = searchParams.get('category');
-  const { data: postList, refetch: updatePostList } = useGetPostList(
-    category ?? '',
-  );
+  let category = isInCategory(searchParams.get('category') ?? '');
+  const postListState = useGetInfinitePostList({
+    size: 2,
+    category: category ?? '',
+  });
+  const updatePostList = postListState.refetch;
+  const {
+    list: postList,
+    intersection,
+    isFetching,
+  } = useIntersectionObserver<PostDetailResponse>(postListState);
 
   useEffect(() => {
     updatePostList();
@@ -20,9 +30,13 @@ export const NewsPage = () => {
     <div className="gap- flex flex-col gap-5">
       <FilterNav />
       <ul className="flex flex-col gap-6 pt-[55px]">
-        {postList?.content.map((data: PostDetailResponse) => (
-          <PostItem data={data} />
+        {postList?.map((data: PostDetailResponse) => (
+          <li key={data.postId}>
+            <PostItem data={data} />
+            <Intersection ref={intersection} />
+          </li>
         ))}
+        {isFetching && <div>로딩 중...</div>}
       </ul>
     </div>
   );
