4d5e6f0a1385b75ab28b317f29120674bd715746
gaeunnlee <gaeunnnleee@gmail.com>
2024-06-12 15:17:32 +0900

    feat: 폼 엑셀 추출 기능 추가

--- src/pages/form/response/_FormResponsePage.tsx
+++ src/pages/form/response/_FormResponsePage.tsx
@@ -4,9 +4,11 @@ import { useGetForm } from '../../../query-hooks/form';
 import { Loader } from '../../../components/Loader';
 import { QuestionBox } from '../id/QuestionBox';
 import { Answer } from './Answer';
+import { API_BASE_URL } from '../../../constant';
 
 export const FormResponsePage = () => {
   const [searchParams] = useSearchParams();
+
   const formID = Number(searchParams.get('id'));
   const {
     data: formInfo,
@@ -15,7 +17,17 @@ export const FormResponsePage = () => {
   } = useGetForm(formID);
 
   return isLoadFormSuccess ? (
-    <FormLayout formInfo={formInfo!}>
+    <FormLayout
+      formInfo={formInfo!}
+      exportButton={
+        <a
+          href={`${API_BASE_URL}/excel/download?fileName=${formInfo.title}&surveyId=${formID}`}
+          className="cursor-pointer underline"
+        >
+          Excel로 저장하기
+        </a>
+      }
+    >
       {formInfo.surveyItems.map(
         ({ surveyItemId: questionID, title, description }) => (
           <QuestionBox title={title} description={description}>
