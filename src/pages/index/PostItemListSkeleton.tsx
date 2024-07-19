73fd3873d038885ed521d3394f85c7632249c247
gaeunnlee <gaeunnnleee@gmail.com>
2024-07-20 00:42:41 +0900

    feat: 메인 Post Suspense와 Skeleton 적용 (#5)

---
+++ src/pages/index/PostItemListSkeleton.tsx
@@ -0,0 +1,21 @@
+import { Box } from '../../components/Box';
+import HorizontalScrollBox from '../../components/HorizontalScrollBox';
+import { SwiperSlide } from 'swiper/react';
+
+export default function PostItemsSkeleton() {
+  return (
+    <HorizontalScrollBox>
+      {Array.from({ length: 2 }).map((_, index) => (
+        <SwiperSlide>
+          <Box
+            key={index}
+            className="skeleton_loading h-[17rem] w-[17rem] flex-col overflow-hidden p-0"
+          >
+            <div className="h-[17rem]" />
+            <div className="box-border h-[4rem] bg-white" />
+          </Box>
+        </SwiperSlide>
+      ))}
+    </HorizontalScrollBox>
+  );
+}
