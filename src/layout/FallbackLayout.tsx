402bae9df8153f1510a7274110fd7d73359e0667
gaeunnlee <gaeunnnleee@gmail.com>
2024-07-19 21:17:03 +0900

    feat: Suspense Fallback 제거 (#5)

--- src/layout/FallbackLayout.tsx
+++ src/layout/FallbackLayout.tsx
@@ -1,10 +1,9 @@
 import { Outlet } from 'react-router-dom';
-import { Loader } from '../components/Loader';
 import { Suspense } from 'react';
 
 export const FallbackLayout = () => (
   <div className="flex h-full w-full flex-col justify-center">
-    <Suspense fallback={<Loader type="clip" />}>
+    <Suspense>
       <Outlet />
     </Suspense>
   </div>
