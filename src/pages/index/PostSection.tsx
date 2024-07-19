ac2091124d7f8145dfe4adae7bf1efb8650ae2dd
gaeunnlee <gaeunnnleee@gmail.com>
2024-07-19 21:13:45 +0900

    feat: 메인 포스트 Skeleton 적용 (#5)

--- src/pages/index/PostSection.tsx
+++ src/pages/index/PostSection.tsx
@@ -4,18 +4,28 @@ import { PostItem } from './PostItem';
 import { Section } from './Section';
 import { useGetPostList } from '../../query-hooks/post';
 import { PostDetailResponse } from '../../api/post/types';
+import { Box } from '../../components/Box';
 
 export const PostSection = () => {
-  const { data: postList } = useGetPostList('');
+  const { data: postList, isFetched } = useGetPostList('');
 
   return (
     <Section title="게시글 둘러보기">
       <HorizontalScrollBox>
-        {postList?.content.map((postData: PostDetailResponse) => (
-          <SwiperSlide key={`post-${postData.postId}`}>
-            <PostItem data={postData} />
-          </SwiperSlide>
-        ))}
+        {isFetched
+          ? postList?.content.map((postData: PostDetailResponse) => (
+              <SwiperSlide key={`post-${postData.postId}`}>
+                <PostItem data={postData} />
+              </SwiperSlide>
+            ))
+          : Array.from({ length: 2 }).map(() => (
+              <SwiperSlide>
+                <Box className="skeleton_loading h-[17rem] w-[17rem] flex-col p-0">
+                  <div className="h-[17rem]" />
+                  <div className="h-[4rem] bg-white" />
+                </Box>
+              </SwiperSlide>
+            ))}
       </HorizontalScrollBox>
     </Section>
   );
