7b10b40df58aa438181a5a643ef7a9965f3340cf
gaeunnlee <gaeunnnleee@gmail.com>
2024-07-20 00:42:41 +0900

    feat: 메인 Post Suspense와 Skeleton 적용 (#5)

--- src/pages/index/PostSection.tsx
+++ src/pages/index/PostSection.tsx
@@ -1,32 +1,14 @@
-import { SwiperSlide } from 'swiper/react';
-import HorizontalScrollBox from '../../components/HorizontalScrollBox';
-import { PostItem } from './PostItem';
+import { Suspense, lazy } from 'react';
 import { Section } from './Section';
-import { useGetPostList } from '../../query-hooks/post';
-import { PostDetailResponse } from '../../api/post/types';
-import { Box } from '../../components/Box';
+import PostItemListSkeleton from './PostItemListSkeleton';
+const PostItemList = lazy(() => import('./PostItemList'));
 
 export const PostSection = () => {
-  const { data: postList, isFetched } = useGetPostList('');
-
   return (
     <Section title="게시글 둘러보기">
-      <HorizontalScrollBox>
-        {isFetched
-          ? postList?.content.map((postData: PostDetailResponse) => (
-              <SwiperSlide key={`post-${postData.postId}`}>
-                <PostItem data={postData} />
-              </SwiperSlide>
-            ))
-          : Array.from({ length: 2 }).map((_, index) => (
-              <SwiperSlide key={`post-${index}`}>
-                <Box className="skeleton_loading h-[17rem] w-[17rem] flex-col p-0">
-                  <div className="h-[17rem]" />
-                  <div className="h-[4rem] bg-white" />
-                </Box>
-              </SwiperSlide>
-            ))}
-      </HorizontalScrollBox>
+      <Suspense fallback={<PostItemListSkeleton />}>
+        <PostItemList />
+      </Suspense>
     </Section>
   );
 };
