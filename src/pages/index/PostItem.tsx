8e47564c1e96532736bc5b6b905258f262948ffd
gaeunnlee <gaeunnnleee@gmail.com>
2024-07-19 21:13:45 +0900

    feat: 메인 포스트 Skeleton 적용 (#5)

--- src/pages/index/PostItem.tsx
+++ src/pages/index/PostItem.tsx
@@ -16,14 +16,14 @@ export const PostItem = ({
       onClick={() => {
         navigate(`/news/${postId}`);
       }}
-      className="h-[17rem] w-[17rem] cursor-pointer flex-col overflow-hidden p-0"
+      className={`skeleton_loading h-[17rem] w-[17rem] cursor-pointer flex-col overflow-hidden p-0`}
     >
       <div
-        className="h-[17rem] bg-slate-300 bg-cover"
+        className="h-[17rem] bg-[#e5e5e5] bg-cover"
         style={{
-          backgroundImage: `url(${postFileResponse[0]?.url})` ?? '',
+          backgroundImage: `url(${postFileResponse[0]?.url ?? ''})`,
         }}
-      ></div>
+      />
       <div className="box-border flex h-[4rem] items-center gap-2 bg-white p-2">
         <img
           className="h-[2rem] w-[2rem] rounded-full border-[1px] border-solid border-[#e7e7e7]"
@@ -31,7 +31,7 @@ export const PostItem = ({
           alt="학생회 로고"
         />
         <div className="flex w-[calc(100%-2.8rem)] flex-col gap-[0.1rem]">
-          <p className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm ">
+          <p className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-sm">
             {title}
           </p>
           <p className="text-xs text-[#848484]">SW융합대학 SW:ing 학생회</p>
