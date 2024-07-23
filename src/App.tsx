03a81eedad09c8861f7fdd024918689df821b47e
gaeunnlee <gaeunnnleee@gmail.com>
2024-07-23 21:22:52 +0900

    feat: React Query Devtools 적용 (#9)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Router from './Router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ModalProvider } from '@components/modal/ModalProvider';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,6 +22,7 @@ function App() {
           <Router />
         </div>
       </ModalProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
