860eda65cd0e103760a1800a58e6b9c9f10bc791
gaeunnlee <gaeunnnleee@gmail.com>
2024-07-28 10:14:46 +0900

    feat: Box cn 유틸 적용 (#10)

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,20 +1,26 @@
 import { HTMLAttributes, ReactNode } from 'react';
-import { handleCustomCSS } from '@utils/handleCustomCSS';
+import { cva, VariantProps } from 'class-variance-authority';
+import { cn } from '@/utils/cn';
 
-type Box = HTMLAttributes<HTMLDivElement> & {
+export const BoxVariants = cva(
+  `flex h-fit w-full rounded-[10px] bg-white p-5 shadow-[1px_2px_10px_0px_rgba(0,0,0,0.1)] overflow-hidden`,
+  {
+    variants: {},
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
+);
+
+interface BoxProps
+  extends HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof BoxVariants> {
   children?: ReactNode;
-  className?: string;
-};
+}
 
-export const boxStyle =
-  'flex h-fit w-full rounded-[10px] bg-white p-5 shadow-[1px_2px_10px_0px_rgba(0,0,0,0.1)]';
-
-export const Box = ({ children, className, ...props }: Box) => {
+export const Box = ({ children, className, ...props }: BoxProps) => {
   return (
-    <div
-      className={className ? handleCustomCSS(boxStyle, className) : boxStyle}
-      {...props}
-    >
+    <div className={cn(BoxVariants(), className)} {...props}>
       {children}
     </div>
   );
