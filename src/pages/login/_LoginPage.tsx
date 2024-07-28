e15e207525d14f503468b8edbe0032b5c0376cb4
gaeunnlee <gaeunnnleee@gmail.com>
2024-07-29 06:59:09 +0900

    feat: 로그인 페이지 Input 공통 컴포넌트 적용 (#10)

--- src/pages/login/_LoginPage.tsx
+++ src/pages/login/_LoginPage.tsx
@@ -1,8 +1,9 @@
 import { Button } from '@components/ui/Button';
-import { Input } from './Input';
 import { LoginInfo } from '@api/login/types';
 import { useState } from 'react';
 import { usePostLoginInfo } from '@query-hooks/login';
+import { setInputChange } from '@/utils/setInputChange';
+import { Input } from './Input';
 
 export const LoginPage = () => {
   const [loginInfo, setLoginInfo] = useState<LoginInfo>({
@@ -16,30 +17,29 @@ export const LoginPage = () => {
     postLoginInfo(loginInfo);
   };
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputChange(e, setLoginInfo);
+  };
+
   return (
     <div className="mt-[-5rem] flex w-full flex-col items-center justify-center">
       <form
-        onSubmit={(e) => {
-          submitLoginInfo(e);
-        }}
+        onSubmit={submitLoginInfo}
         className="flex h-fit w-full flex-col items-center gap-5 p-5 text-[#C4C4C4]"
       >
         <h1 className="font-SejongHospitalBold text-4xl text-primary">단통</h1>
         <Input
-          onChange={(e) => {
-            setLoginInfo((prev) => ({ ...prev, studentId: e.target.value }));
-          }}
           label="Student ID"
+          name="studentId"
           maxLength={8}
-        >
-          <span>@dankook.ac.kr</span>
-        </Input>
+          onChange={handleInputChange}
+          inputContent={<span>@dankook.ac.kr</span>}
+        />
         <Input
-          onChange={(e) => {
-            setLoginInfo((prev) => ({ ...prev, password: e.target.value }));
-          }}
           label="Password"
+          name="password"
           type="password"
+          onChange={handleInputChange}
         />
         <Button size="full" content="로그인" />
       </form>
