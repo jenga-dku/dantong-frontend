36f1ad22acf4dde466161cd128014e2c681828aa
gaeunnlee <gaeunnnleee@gmail.com>
2024-07-29 19:36:35 +0900

    design: Skeleton background 색상 설정

--- src/pages/index/PostItemListSkeleton.tsx
+++ src/pages/index/PostItemListSkeleton.tsx
@@ -9,7 +9,7 @@ export default function PostItemsSkeleton() {
         <SwiperSlide>
           <Box
             key={index}
-            className="skeleton h-[17rem] w-[17rem] flex-col overflow-hidden p-0"
+            className="skeleton h-[17rem] w-[17rem] flex-col overflow-hidden bg-[#e5e5e5] p-0"
           >
             <div className="h-[17rem]" />
             <div className="box-border h-[4rem] bg-white" />
