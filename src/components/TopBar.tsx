c7dbf2335968add5d8fd5a6a692cee5b74fcbbf0
gaeunnlee <gaeunnnleee@gmail.com>
2024-08-17 03:54:00 +0900

    design: PWA 레이아웃 수정

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -8,22 +8,27 @@ export const TopBar = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="fixed top-0 z-50 box-border flex h-[40px] w-full max-w-[400px] items-center justify-between bg-[#EBF4FF] px-5 pb-3 pt-5 text-[#AFAFAF]">
-      {isBackButtonVisible ? (
-        <button
-          className="text-2xl"
-          onClick={() => {
-            navigate(-1);
-          }}
-        >
-          <FaAngleLeft />
-        </button>
-      ) : (
-        <h1 className="font-SejongHospitalBold text-2xl">단통</h1>
-      )}
-      {isNotificationButtonVisible && (
-        <IoNotifications className="text-3xl text-[#AFAFAF]" />
-      )}
-    </div>
+    <>
+      <div
+        style={{ paddingTop: 'calc(1rem + env(safe-area-inset-top))' }}
+        className="fixed top-0 z-50 box-border flex h-[40px] w-full max-w-[400px] items-center justify-between bg-[#EBF4FF] px-5 pb-3 text-[#AFAFAF]"
+      >
+        {isBackButtonVisible ? (
+          <button
+            className="text-2xl"
+            onClick={() => {
+              navigate(-1);
+            }}
+          >
+            <FaAngleLeft />
+          </button>
+        ) : (
+          <h1 className="font-SejongHospitalBold text-2xl">단통</h1>
+        )}
+        {isNotificationButtonVisible && (
+          <IoNotifications className="text-3xl text-[#AFAFAF]" />
+        )}
+      </div>
+    </>
   );
 };
