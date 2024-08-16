97d7ad365c0d2d41903642fd4385a0843283dff8
gaeunnlee <gaeunnnleee@gmail.com>
2024-08-16 22:23:56 +0900

    feat: FCM 토큰 저장 로직 구현 (#14)

--- src/pages/notification/_NotificationPage.tsx
+++ src/pages/notification/_NotificationPage.tsx
@@ -1,7 +1,48 @@
 import { Box } from '@/components/ui/Box';
 import { Button } from '@/components/ui/Button';
+import { useEffect, useState } from 'react';
+import { requestFcmToken, messaging } from '@/utils/firebaseUtils';
+import { onMessage } from 'firebase/messaging';
+import toast from 'react-hot-toast';
+import { useTopBarStore } from '@/stores/topBar-stores';
 
 export const NotificationPage = () => {
+  const notificationToken = (function () {
+    return localStorage.getItem('notificationToken');
+  })();
+  const [isNotificationOn, setIsNotificationOn] = useState(!!notificationToken);
+
+  const fetchFcmToken = async () => {
+    if (!notificationToken && isNotificationOn) {
+      await requestFcmToken()
+        .then((res) => {
+          localStorage.setItem('notificationToken', res!);
+          toast('알림을 설정하였습니다.');
+          onMessage(messaging, (payload) => {
+            console.log(payload);
+            toast(payload!.notification!.body!);
+          });
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    } else if (notificationToken && !isNotificationOn) {
+      toast('알림을 해제하였습니다.');
+      localStorage.setItem('notificationToken', '');
+    }
+  };
+
+  useEffect(() => {
+    fetchFcmToken();
+  }, [isNotificationOn]);
+
+  useEffect(() => {
+    useTopBarStore.setState({
+      isBackButtonVisible: true,
+      isNotificationButtonVisible: false,
+    });
+  }, []);
+
   return (
     <Box className="flex-col gap-7">
       <div
@@ -13,7 +54,10 @@ export const NotificationPage = () => {
           <input
             type="checkbox"
             className="toggle toggle-primary"
-            defaultChecked
+            defaultChecked={!!notificationToken}
+            onClick={() => {
+              setIsNotificationOn((prev) => !prev);
+            }}
           />
         </div>
       </div>
