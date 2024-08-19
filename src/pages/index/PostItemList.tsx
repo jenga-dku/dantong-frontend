759b9a97118c8ea09c8c1454ec3f8025cd02188d
gaeunnlee <gaeunnnleee@gmail.com>
2024-08-19 23:38:09 +0900

    feat: 메인화면 슬라이드 스와이프 타입 변경

--- src/pages/index/PostItemList.tsx
+++ src/pages/index/PostItemList.tsx
@@ -1,19 +1,39 @@
-import { SwiperSlide } from 'swiper/react';
-import HorizontalScrollBox from '@components/HorizontalScrollBox';
+import 'swiper/css';
+import 'swiper/css/pagination';
 import { PostItem } from './PostItem';
 import { useGetPostList } from '@query-hooks/post';
 import { PostDetailResponse } from '@api/post/types';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { ReactNode } from 'react';
 
 export default function PostItemList() {
   const { data: postList } = useGetPostList('');
 
   return (
-    <HorizontalScrollBox>
+    <PostListSwiperSlide>
       {postList?.content.map((postData: PostDetailResponse) => (
         <SwiperSlide key={`post-${postData.postId}`}>
           <PostItem data={postData} />
         </SwiperSlide>
       ))}
-    </HorizontalScrollBox>
+    </PostListSwiperSlide>
   );
 }
+
+export const PostListSwiperSlide = ({ children }: { children: ReactNode }) => (
+  <Swiper
+    slidesPerView={1}
+    breakpoints={{
+      345: {
+        slidesPerView: 1.15,
+      },
+      380: {
+        slidesPerView: 1.25,
+      },
+    }}
+    spaceBetween={10}
+    className="swiperPerView"
+  >
+    {children}
+  </Swiper>
+);
