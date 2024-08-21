13e9a07c666117d9834b52f8563fa0ba78bec3d5
gaeunnlee <gaeunnnleee@gmail.com>
2024-08-22 06:51:14 +0900

    design: 폼 질문 추가 버튼 위치 수정 (#15)

--- src/pages/form/upload/components/button/AddQuestionButton.tsx
+++ src/pages/form/upload/components/button/AddQuestionButton.tsx
@@ -11,7 +11,7 @@ export const AddQuestionButton = ({
           type: 'createQuestion',
         });
       }}
-      className="btn fixed bottom-[70px] flex h-[60px] w-[60px] items-center justify-center rounded-full bg-white p-3 text-[30rem] font-bold text-primary shadow-[1px_2px_10px_0px_rgba(0,0,0,0.1)]"
+      className="btn fixed bottom-[calc(env(safe-area-inset-bottom)+70px)] flex h-[60px] w-[60px] items-center justify-center rounded-full bg-white p-3 text-[30rem] font-bold text-primary shadow-[1px_2px_10px_0px_rgba(0,0,0,0.1)]"
     >
       <TbPlus size={30} />
     </button>
